aa24f3411846e31f78887ce30e30b7f10a182d34
mmirwaldt <michael.mirwaldt@gmail.com>
2021-02-20 02:51:46 +0100

    finally solved part two after struggling hours with string.replace(...) and escape sequences which interfered each other in silly ways

--- src/main/java/net/mirwaldt/CharCounterMain.java
+++ src/main/java/net/mirwaldt/CharCounterMain.java
@@ -10,13 +10,39 @@ public class CharCounterMain {
     public static void main(String[] args) throws IOException {
         final List<String> lines = Files.readAllLines(Path.of("input.txt"), StandardCharsets.US_ASCII);
         final CharCounter charCounter = new DefaultCharCounter();
+
         long codeCharsCount = 0;
         long stringCharsCount = 0;
         for (String line : lines) {
             codeCharsCount += charCounter.countCodeCharacters(line);
             stringCharsCount += charCounter.countStringCharacters(line);
         }
-        final long difference = codeCharsCount - stringCharsCount;
-        System.out.println(difference); // result - 1371
+        final long partOneDifference = codeCharsCount - stringCharsCount;
+        System.out.println(partOneDifference); // result - 1371
+
+        long codeCharsCountOfEncodedString = 0;
+        for (String line : lines) {
+            final String escapedLine = escape(line);
+            codeCharsCountOfEncodedString += charCounter.countCodeCharacters(escapedLine);
+        }
+        final long partTwoDifference = codeCharsCountOfEncodedString - codeCharsCount;
+        System.out.println(partTwoDifference);  // result - 2117
+    }
+
+    public static String escape(String line) {
+        /*
+         * We first mark all escape sequences and then escape them then again to avoid confusion.
+         * We can do that because numbers only occur after \x
+         * in hexadecimal notation and nowhere else - and that's our luck!
+         */
+        return "\"" + line.replace("\\\\", "\\0")
+                .replace("\\x", "\\1")
+                .replace("\\\"","\\2")
+                .replace("\"","\\3")
+                .replace("\\0", "\\\\" + "\\\\")
+                .replace("\\1", "\\" + "\\x")
+                .replace("\\2", "\\\\\\\"")
+                .replace("\\3", "\\\"")
+                + "\"";
     }
 }
